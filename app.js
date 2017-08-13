ce68148b9a58138a28ac26daf5f1408d3b03940a
edniltonmartins <ednilton17@gmail.com>
2017-08-13 18:36:34 -0300

    Primeiro estudos de ECMA6

---
+++ app.js
@@ -0,0 +1,4 @@
+import {Negociacao} from "./Negociacao";
+var negociacao = new Negociacao();
+
+console.log(negociacao);
---
+++ js/model/Negociacao.js
@@ -0,0 +1,21 @@
+class Negociacao{
+    constructor(data, valor, quantidade){
+        this._data = new Date(data.getTime());
+        this._valor = valor;
+        this._quantidade = quantidade;
+
+        Object.freeze(this);
+    }
+
+    get data(){
+        return this._data;
+    }
+
+    get valor(){
+        return this._valor;
+    }
+
+    get quantidade(){
+        return this._quantidade;
+    }
+}
